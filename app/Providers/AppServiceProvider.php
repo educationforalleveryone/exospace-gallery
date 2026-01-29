32f7e05b79d79748300111ab4f37c89722f1e619
Life Saver <educationforalleveryone@gmail.com>
2026-01-30 00:30:09 +0500

    mac update 1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // This forces the site to use HTTPS when live, fixing the broken styles and login error.
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
