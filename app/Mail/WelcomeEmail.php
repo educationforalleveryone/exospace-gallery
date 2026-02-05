0c749f9eb15c2738a00124aacdb98a149d417f93
LifeSaver <lifesaver@save.life.com>
2026-02-06 00:10:21 +0500

    queue emails + switch to resend api

--- app/Mail/WelcomeEmail.php
+++ app/Mail/WelcomeEmail.php
@@ -4,12 +4,13 @@ namespace App\Mail;
 
 use App\Models\User;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;  // ← ADD THIS LINE
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class WelcomeEmail extends Mailable
+class WelcomeEmail extends Mailable implements ShouldQueue  // ← ADD "implements ShouldQueue"
 {
     use Queueable, SerializesModels;
 
