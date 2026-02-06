05b4ca6c64468e2645d191eb24b96ecd92ce848e
LifeSaver <lifesaver@save.life.com>
2026-02-06 05:34:16 +0500

    updated docker for worker

--- docker-start.sh
+++ docker-start.sh
@@ -15,5 +15,8 @@ else
     sed -i 's/server {/server {\n    client_max_body_size 50M;/g' /assets/nginx.template.conf
 fi
 
-# 3. Start PHP-FPM with custom config and Nginx
+# 3. START THE QUEUE WORKER (runs in background)
+php /app/artisan queue:work --tries=3 --timeout=90 --sleep=3 &
+
+# 4. Start PHP-FPM with custom config and Nginx
 node /assets/scripts/prestart.mjs /assets/nginx.template.conf /nginx.conf && (php-fpm -y /assets/php-fpm.conf -d upload_max_filesize=50M -d post_max_size=50M -d memory_limit=512M & nginx -c /nginx.conf)
